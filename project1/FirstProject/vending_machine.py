60635787f262f32ebc6fdea13269bd5fbd099626
leehanseock <leehanseock@gmail.com>
2024-09-04 13:00:39 +0900

    현금 투입 함수 출력방식 변경

--- project1/FirstProject/vending_machine.py
+++ project1/FirstProject/vending_machine.py
@@ -2,15 +2,25 @@
 #if __name__ == "__main__":
 
 # 딕셔너리를 사용한 메뉴 생성
-menu = {'콜라':500, '사이다':500, '물':800, '파워에이드':1000}
+menu = {1:{'콜라':500}, 2:{'사이다':500}, 3:{'물':800}, 4:{'파워에이드':1000}}
 # menu_name = list(menu.keys())
 
 # 메뉴 출력 함수
 def print_menu():
     print("🥤한석벤딩🥤")
-    for k, v in menu.items():
-        print(f'{k}: {v}원', end=' / ')
+    for inner_dict in menu.values():
+        for item, price in inner_dict.items():
+            print(f'|{item}: {price}원|', end=' ')
+print_menu()
 
 # 현금 투입 함수
 def insert_cash(money):
     print(f"현재 잔액 : {m}원", money) # 최초잔액 표시
+
+# 메뉴 선택 함수
+def make_a_choice(number):
+    number =input("구매하고 싶은 음료 번호를 선택하여 주십시오:")
+    try :
+        print('ok')
+    except KeyError as e:
+        print("메뉴에 있는 번호를 선택해 주십시오.")
