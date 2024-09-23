5fc7826e0db6636bab46ff15d74f314935b8c057
leehanseock <leehanseock@gmail.com>
2024-09-23 13:39:21 +0900

    9월 23일 4강

---
+++ project1/FirstProject/0923.py
@@ -0,0 +1,30 @@
+# L = [1,2,3,4,5,6,7,8,9]
+# # try:
+# #     n = L.index(-1)
+# #     print(n)
+# # except:
+# #     print("nothing")
+#
+# n = L.index(1)
+# print(n)
+# print("End")
+
+# #모든 3의 위치를 표시하고 마지막에 End 출력
+# L = [1, 9, 3, 4, 3, 2]
+# for i in range(len(L)):
+#     if L[i] == 3:
+#         print(i)
+#
+# print("End")
+
+# s = '나는 \'안녕\'이라고 말했다'
+# s2 = "나는 '안녕'이라고 말했다"
+# print(s)
+# print(s2)
+#
+# s3 = '''
+# 줄 바꾸기 연습
+#
+# 오호라
+# '''
+# print(s3)
