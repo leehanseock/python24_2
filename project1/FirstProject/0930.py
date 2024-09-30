43ef9772084e7d87d724d8992ee83cc631b97b82
leehanseock <leehanseock@gmail.com>
2024-09-30 10:33:47 +0900

    9월 30일 5강 함축예제2

--- project1/FirstProject/0930.py
+++ project1/FirstProject/0930.py
@@ -1,4 +1,8 @@
-num = [0,1,2,3]
-english = ['zero', 'one', 'two']
-sdict = {n:e for n, e in zip(num, english)}
-print(sdict)
+# num = [0,1,2,3]
+# english = ['zero', 'one', 'two']
+# sdict = {n:e for n, e in zip(num, english)}
+# print(sdict)
+
+s = 'oh my god!'
+q = list(s)
+print(q)
