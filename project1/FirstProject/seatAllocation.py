dc6f1d2bd6b8d63ea10658ee4142a8d29b46a9af
leehanseock <leehanseock@gmail.com>
2024-10-02 00:28:15 +0900

    넘파이 라이브러리를 활용한 자리배정 생성기 + 빈 좌석 처리

--- project1/FirstProject/seatAllocation.py
+++ project1/FirstProject/seatAllocation.py
@@ -5,13 +5,22 @@
 # 넘파이 라이브러리 임포트
 import numpy as np
 
-# 좌석 배치 전의 학생이름 리스트 생성('before')
+# 좌석 배치 전의 학생이름(28명) 리스트 생성('before')
 before = ['도라에몽', '퉁퉁이', '진구', '비실이', '이슬이',
             '짱구', '훈이', '맹구', '철수', '유리',
             '한지우', '웅', '로사', '로이', '나옹',
             '신태일', '매튜', '한소라', '장한솔', '이미나',
             '정석', '리키', '신나리', '샐리', '셜록스',
-            '세인트', '이누야샤', '유가영', '금강', '싯포']
+            '세인트', '이누야샤', '유가영']
+print('학생리스트 : ', before)
+print('총 학생 수 : ',len(before))
+print('빈 자리 수 : ', 30 - len(before))
+
+# 학생이름 수가 30개 미만일 때 null 값 처리
+if len(before) <= 30 :
+    emptyNum = 30 - len(before)
+    for e in range(emptyNum) :
+        before.append("빈 좌석")
 
 # 파이썬 리스트를 넘파이 배열로 변환하기
 before_arr = np.array(before)
@@ -60,6 +69,7 @@ endList =["\t", " \t", "  \t", "   \t"]
 #         print(after[k], end=endList[0])
 # print()
 #
+print('~좌석배치 결과~')
 for n in range(0, 30, 10):
     for i in range(n, n + 10):
         if len(after[i]) == 1:
