1f49c9506052112a76e14ba11ce5e953db99fc7e
leehanseock <leehanseock@gmail.com>
2024-11-11 12:26:06 +0900

    11월 11일 자판기 과제2 주석제거

--- project1/VendingMachine/vendingMachine.py
+++ project1/VendingMachine/vendingMachine.py
@@ -2,32 +2,10 @@ from project1.VendingMachine.beverage import Beverage
 
 class VendingMachine:
     def __init__(self):
-        # self.__number = number
-        # self.__total = total
         self.Menu  = {1: Beverage('콜라',900, 10),
                       2: Beverage("사이다", 1000, 10)}
 
         self.inputMoney = 0
-
-        #음료 초기화
-        # self.list = []
-        # num = input("몇개의 음료를 판매할거니?")
-        # for i in range(num):
-        #     name = input("제품명: ")
-        #     price = int(input("가격: "))
-        #     count = int(input("재고량: "))
-        #     item = Beverage(name, price, count)
-        #     self.list.append(item)
-
-
-    # def ManageStock(self, *belist):
-    #     num = input("판매할 음료 갯수")
-    #     while(i<num):
-    #         i = 0
-    #         name = input("제품명: ")
-    #         price = input("가격: ")
-    #         count = input("재고량: ")
-    #         beverage[i+1] = beverage()
 
     def printMenu(self):
         for key, value in self.Menu.items():
@@ -38,8 +16,6 @@ class VendingMachine:
                 "" if value.getCount()>=0 else "품절"
             )
             print(str + "\n")
-        # for j in list:
-        #     print(f"[{j+1}] 제품명:{j.getName} / 가격:{j.getPrice} / 재고량:{j.getCount}")
 
     def InputMoney(self, money:int):
         self.inputMoney = money
@@ -51,16 +27,6 @@ class VendingMachine:
 
     def PrintInputMoney(self):
         print("투입된 금액: "+str(self.inputMoney)+"원")
-    #
-    # # 음료 제공
-    # def Give(self, k):
-    #     print(f"선택하신 음료 {Beverage[k]}가 나옵니다. 음료수를 받아주세요.")
-    #     Beverage[k].sale
-    #
-    # # 상품 선택
-    # def Choose(self, choice):
-    #     choice = input("주문할 음료 번호를 입력해주십시오: ")
-    #     self.give(choice - 1)
 
     # 메뉴 선택
     def ChooseMenu(self):
